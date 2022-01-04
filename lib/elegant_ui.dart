05adef84398eac6047ca7bd1007ca8175e1c0399
baimamboukar <baimamboukar@gmail.com>
2022-01-04 12:19:29 +0100

    init projecr

--- lib/elegant_ui.dart
+++ lib/elegant_ui.dart
@@ -3,11 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class ElegantUi {
+class ElegantUI {
   void main(List<String> args) {
-    runApp(MaterialApp(
-      home: ElegantUiPage(),
-    ));
+    runApp(const MaterialApp(home: Buttons()));
   }
 
   static const MethodChannel _channel = MethodChannel('elegant_ui');
@@ -20,5 +18,23 @@ class ElegantUi {
   static Widget button(Color color, String text) {
     return Container(
         width: double.infinity, color: color, height: 50, child: Text(text));
+  }
+
+  static Widget input(Color color, double border) {
+    return TextFormField();
+  }
+}
+
+class Buttons extends StatelessWidget {
+  const Buttons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        ElegantUI.button(Colors.red, 'Action'),
+      ],
+    ));
   }
 }
